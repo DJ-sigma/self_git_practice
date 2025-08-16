ae087a8ea834c5f148c54adc9246370167b6db88
S K SAHU <sksahutelco@gmail.com>
2025-08-16 16:22:20 +0530

    2nd commit testing - saumya

--- azurerm_resource_group/main.tf
+++ azurerm_resource_group/main.tf
@@ -1,5 +1,11 @@
-resource "azurerm_resource_group" "rg_todo" {
-    name     = var.resource_group_name
-    location = var.resource_group_location
+resource "azurerm_resource_group" "rg_todo2" {
+    name     = "hero"
+    location = "West Europe""
     
+}
+
+resource "azurerm_resource_group" "rg_todo1" {
+    name     = "hero2"
+    location = "West Europe"
+
 }
