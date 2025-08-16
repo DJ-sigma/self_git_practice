5d54fa7bb0eeefb8af4737c58a640d64589a8ae1
S K SAHU <sksahutelco@gmail.com>
2025-08-16 16:57:56 +0530

    correction as required

--- azurerm_resource_group/main.tf
+++ azurerm_resource_group/main.tf
@@ -1,7 +1,17 @@
+resource "azurerm_resource_group" "rg_todo" {
+    name     = var.resource_group_name
+    location = var.resource_group_location
+    
+}
+
+resource "azurerm_resource_group" "rg_new" {
+    name     = "soumya_1"
+    location = var.resource_group_location
+    
+}
 resource "azurerm_resource_group" "rg_todo2" {
     name     = "hero"
-    location = "West Europe""
-    
+    location = "West Europe"
 }
 
 resource "azurerm_resource_group" "rg_todo1" {
